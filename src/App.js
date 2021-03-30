f20c65c05904c96e8fed217b9f81e170ae96e784
chibie-code <76469653+chibie-code@users.noreply.github.com>
2021-03-29 18:18:59 -0700

    Starting of 'Unsafe Space Chatroom App'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Container, Header, Content, Footer, Sidebar, Button, Icon, Input, InputGroup } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
 
 function App() {
+  const styles = {
+    appContainer: {
+      width: '100%',
+      border: 'solid 1px darkred'
+    },
+    mainContainer: {
+      height: '100vh',
+      overflow: 'hidden'
+    },
+    header: {
+      border: 'solid 1px lime',
+      marginBottom: 'auto',
+      width: '100%',
+      textAlign: 'center'
+    },
+    mainContent: {
+      overflowY: 'auto'
+    },
+    footer: {
+      marginTop: 'auto',
+      width: '100%',
+      textAlign: 'center'
+    },
+    inputGroup: {
+      
+    },
+    inputBox: {
+      marginLeft: '0 2em',
+      rounded: '0'
+    },
+    sidebar: {
+      height: '100vh',
+      border: 'solid 1px',
+      display: 'none'
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="" style={styles.appContainer}>
+
+      <Sidebar style={styles.sidebar}></Sidebar>
+
+      <Container style={styles.mainContainer}>
+      <Header style={styles.header}>
+      <Button>Message Input Section</Button>  
+        Header
+      </Header>
+      
+      <Content style={styles.mainContent}>
+        Content
+
+        
+
+      </Content>
+      
+      <Footer style={styles.footer}>
+
+        <Container>
+          <InputGroup inside size="lg" placeholder="Message" style={styles.inputGroup}>
+            <Input style={styles.inputBox}/>
+            <InputGroup.Button>
+              <i class="fas fa-paper-plane"></i>
+            </InputGroup.Button>
+          </InputGroup>
+        </Container>
+        
+      </Footer>
+
+      </Container>
+
+    </Container>
   );
 }
 
---
+++ src/src/App.js
@@ -0,0 +1,22 @@
+// import 'resuite/dist/styles/resuite-default.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. And test!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
